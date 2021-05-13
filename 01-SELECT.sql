c881e8c4afc3ea0a51f897e1e016e5e7bd94e49c
송정화 <s00149s@naver.com>
2021-05-13 17:18:51 +0900

    JOIN

--- 01-SELECT.sql
+++ 01-SELECT.sql
@@ -298,5 +298,42 @@ SELECT first_name, department_id,
 FROM employees
 ORDER BY team;
 
+-----------------------------
+-- 연습문제 1
+-----------------------------
+SELECT first_name || ' ' || last_name 이름,
+    salary 월급,
+    phone_number 전화번호,
+    hire_date 입사일
+FROM employees
+ORDER BY hire_date ASC;
+
+-- 연습문제 2
+SELECT job_title,
+    max_salary
+FROM jobs
+ORDER BY max_salary DESC;
+
+-- 연습문제 3
+SELECT first_name,
+    salary,
+    manager_id,
+    commission_pct,
+    salary
+FROM employees
+WHERE manager_id IS NOT NULL
+AND commission_pct IS NULL
+AND salary > 3000;
+
+-- 연습문제 4
+SELECT job_title,
+    max_salary
+FROM jobs
+WHERE max_salary >= 10000
+ORDER BY max_salary DESC;
+
+-- 연습문제 5
+
+
 
 
